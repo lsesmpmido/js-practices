78736f33e1abcb6ca23118e02f7a1d526e4db643
r.kawano <r.kawano@genbasupport.com>
2024-12-03 14:39:07 +0900

    fizzbuzz.jsを追加
    ・3の倍数のとき
    ・5の倍数のとき
    ・3の倍数でも5の倍数でもないとき
    の3パターンで処理をするようにコードを作成

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+for (let i = 1; i <= 20; i++) {
+  let text = "";
+
+  if (i % 3 === 0) {
+    text += "Fizz";
+  }
+  if (i % 5 === 0) {
+    text += "Buzz";
+  }
+
+  console.log(text || i.toString());
+}
