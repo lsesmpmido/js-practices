fec4ab2d8c5bf45ff93a93819c4a44a0a598b270
r.kawano <r.kawano@genbasupport.com>
2024-12-05 13:36:12 +0900

    ARGV.m=1のときに、'ARGV.m-1'がfalseとして処理されないように修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -6,16 +6,16 @@ import { endOfMonth } from "date-fns";
 const ARGV = minimist(process.argv.slice(2));
 const today = new Date();
 const YEAR = ARGV.y || today.getFullYear();
-const MONTH = ARGV.m - 1 || today.getMonth();
-const first_day = new Date(YEAR, MONTH, 1);
+const MONTH = ARGV.m || today.getMonth() + 1;
+const first_day = new Date(YEAR, MONTH - 1, 1);
 const END_OF_MONTH = endOfMonth(first_day).getDate();
 
-console.log("      " + (MONTH + 1) + "月 " + YEAR);
+console.log("      " + MONTH + "月 " + YEAR);
 console.log("日 月 火 水 木 金 土");
 process.stdout.write("   ".repeat(first_day.getDay()));
 for (let day = 1; day <= END_OF_MONTH; day++) {
   process.stdout.write(day.toString().padStart(2) + " ");
-  if (new Date(YEAR, MONTH, day).getDay() === 6) {
+  if (new Date(YEAR, MONTH - 1, day).getDay() === 6) {
     process.stdout.write("\n");
   }
 }
