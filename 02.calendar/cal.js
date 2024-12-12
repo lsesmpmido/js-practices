318c342dced72ed5297efb3276dafbd553a1a0e0
r.kawano <r.kawano@genbasupport.com>
2024-12-12 14:37:44 +0900

    オブジェクトのプロパティを切り離さないように修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -8,12 +8,12 @@ const today = new Date();
 const YEAR = argv.y || today.getFullYear();
 const MONTH = argv.m || today.getMonth() + 1;
 const firstDay = new Date(YEAR, MONTH - 1, 1);
-const END_OF_MONTH = endOfMonth(firstDay).getDate();
+const lastDay = endOfMonth(firstDay);
 
 console.log(`      ${MONTH}月 ${YEAR}`);
 console.log("日 月 火 水 木 金 土");
 process.stdout.write("   ".repeat(firstDay.getDay()));
-for (let day = 1; day <= END_OF_MONTH; day++) {
+for (let day = 1; day <= lastDay.getDate(); day++) {
   process.stdout.write(day.toString().padStart(2) + " ");
   if ((firstDay.getDay() + day - 1) % 7 === 6) {
     process.stdout.write("\n");
